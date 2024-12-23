b0f95b49ff823c1048f0da6efaa4955d4364e6f8
Tyfyter <tyfyter2616@gmail.com>
2024-12-22 20:06:08 -0700

    Create UnstableHooking.cs

---
+++ UnstableHooking.cs
@@ -0,0 +1,19 @@
+﻿using MonoMod.Cil;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+
+namespace PegasusLib {
+	/// <summary>
+	/// For hooking methods which 
+	/// </summary>
+	public static class UnstableHooking {
+		public static event ILContext.Manipulator IL_Main_DoDraw {
+			add => Main.QueueMainThreadAction(() => IL_Main.DoDraw += value);
+			remove => Main.QueueMainThreadAction(() => IL_Main.DoDraw -= value);
+		}
+	}
+}
