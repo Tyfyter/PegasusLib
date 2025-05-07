0eaec3c8ae9dacc12686296aecffe250a3ebffd8
Tyfyter <tyfyter2616@gmail.com>
2025-05-06 22:06:47 -0600

    Improvements

--- LanguageTree.cs
+++ LanguageTree.cs
@@ -1,10 +1,12 @@
 ﻿using System.Collections.Concurrent;
+using System.Linq;
 using Terraria.Localization;
 
 namespace PegasusLib {
 	public class LanguageTree(LocalizedText value = default) : ConcurrentDictionary<string, LanguageTree> {
 		public LocalizedText value = value;
 		public string TextValue => value.Value;
+		public LocalizedText[] Children => Values.Select(tree => tree.value).ToArray();
 		public LanguageTree GetOrCreate(string key) => GetOrAdd(key, _ => new());
 		public LanguageTree Find(string path, bool create = true) {
 			int splitIndex = path.IndexOf('.');
