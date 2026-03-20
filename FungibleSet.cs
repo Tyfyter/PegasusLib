bd14e211b2ce18226989b3070f276a158d5c546b
Tyfyter <tyfyter2616@gmail.com>
2026-03-19 20:45:29 -0600

    Update FungibleSet.cs

--- FungibleSet.cs
+++ FungibleSet.cs
@@ -34,43 +34,23 @@ namespace PegasusLib {
 				this[entry.Key] += entry.Value;
 			}
 		}
+		public int Add(T key, int value = 1) => this[key] += value;
+		public int Remove(T key, int value = 1) => -(this[key] -= value);
 		public int Total => Entries.Values.Sum();
 		public ICollection<T> Keys => Entries.Keys;
 		public ICollection<int> Values => Entries.Values;
 		public int Count => Entries.Count;
 		public bool IsReadOnly => ((ICollection<KeyValuePair<T, int>>)Entries).IsReadOnly;
-		public void Add(T key, int value) {
-			Entries.Add(key, value);
-		}
-		public void Add(KeyValuePair<T, int> item) {
-			((ICollection<KeyValuePair<T, int>>)Entries).Add(item);
-		}
-		public void Clear() {
-			Entries.Clear();
-		}
-		public bool Contains(KeyValuePair<T, int> item) {
-			return Entries.Contains(item);
-		}
-		public bool ContainsKey(T key) {
-			return Entries.ContainsKey(key);
-		}
-		public void CopyTo(KeyValuePair<T, int>[] array, int arrayIndex) {
-			((ICollection<KeyValuePair<T, int>>)Entries).CopyTo(array, arrayIndex);
-		}
-		public IEnumerator<KeyValuePair<T, int>> GetEnumerator() {
-			return Entries.GetEnumerator();
-		}
-		public bool Remove(T key) {
-			return Entries.Remove(key);
-		}
-		public bool Remove(KeyValuePair<T, int> item) {
-			return ((ICollection<KeyValuePair<T, int>>)Entries).Remove(item);
-		}
-		public bool TryGetValue(T key, out int value) {
-			return Entries.TryGetValue(key, out value);
-		}
-		IEnumerator IEnumerable.GetEnumerator() {
-			return Entries.GetEnumerator();
-		}
+		void ICollection<KeyValuePair<T, int>>.Add(KeyValuePair<T, int> item) => ((ICollection<KeyValuePair<T, int>>)Entries).Add(item);
+		public void Clear() => Entries.Clear();
+		bool ICollection<KeyValuePair<T, int>>.Contains(KeyValuePair<T, int> item) => Entries.Contains(item);
+		bool IDictionary<T, int>.ContainsKey(T key) => Entries.ContainsKey(key);
+		void ICollection<KeyValuePair<T, int>>.CopyTo(KeyValuePair<T, int>[] array, int arrayIndex) => ((ICollection<KeyValuePair<T, int>>)Entries).CopyTo(array, arrayIndex);
+		public IEnumerator<KeyValuePair<T, int>> GetEnumerator() => Entries.GetEnumerator();
+		bool IDictionary<T, int>.Remove(T key) => Entries.Remove(key);
+		bool ICollection<KeyValuePair<T, int>>.Remove(KeyValuePair<T, int> item) => ((ICollection<KeyValuePair<T, int>>)Entries).Remove(item);
+		bool IDictionary<T, int>.TryGetValue(T key, out int value) => Entries.TryGetValue(key, out value);
+		IEnumerator IEnumerable.GetEnumerator() => Entries.GetEnumerator();
+		void IDictionary<T, int>.Add(T key, int value) => Entries.Add(key, value);
 	}
 }
