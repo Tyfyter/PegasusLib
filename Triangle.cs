a3ffc55ba0f684f4e57b77bb3157dbedcfe8be74
Tyfyter <tyfyter2616@gmail.com>
2026-06-09 18:37:04 -0600

    Update Triangle.cs

--- Triangle.cs
+++ Triangle.cs
@@ -1,19 +1,21 @@
-﻿using Microsoft.Xna.Framework;
-using System;
+﻿using System;
 using Terraria;
 
 namespace PegasusLib {
-	public readonly struct Triangle(Vector2 a, Vector2 b, Vector2 c) {
-		public readonly Vector2 a = a;
-		public readonly Vector2 b = b;
-		public readonly Vector2 c = c;
+	public struct Triangle(Vector2 a, Vector2 b, Vector2 c) {
+		public Vector2 a = a;
+		public Vector2 b = b;
+		public Vector2 c = c;
+		public readonly float Area => Math.Abs(a.X * (b.Y - c.Y) + b.X * (c.Y - a.Y) + c.X * (a.Y - b.Y)) * 0.5f;
+		public readonly bool IsDegenerate => float.Abs(Area) <= float.Epsilon;
 		public readonly bool Intersects(Rectangle rect) {
-			return Collision.CheckAABBvLineCollision2(rect.TopLeft(), rect.Size(), a, b) ||
-			Collision.CheckAABBvLineCollision2(rect.TopLeft(), rect.Size(), b, c) ||
-			Collision.CheckAABBvLineCollision2(rect.TopLeft(), rect.Size(), c, a) ||
-			Contains(rect.TopLeft());
+			if (Collision.CheckAABBvLineCollision2(rect.TopLeft(), rect.Size(), a, b)) return true;
+			if (Collision.CheckAABBvLineCollision2(rect.TopLeft(), rect.Size(), b, c)) return true;
+			if (Collision.CheckAABBvLineCollision2(rect.TopLeft(), rect.Size(), c, a)) return true;
+			return Contains(rect.TopLeft());
 		}
 		public readonly bool Contains(Vector2 point) {
+			if (IsDegenerate) return false;
 			bool b0 = Vector2.Dot(new Vector2(point.X - a.X, point.Y - a.Y), new Vector2(a.Y - b.Y, b.X - a.X)) > 0;
 			bool b1 = Vector2.Dot(new Vector2(point.X - b.X, point.Y - b.Y), new Vector2(b.Y - c.Y, c.X - b.X)) > 0;
 			bool b2 = Vector2.Dot(new Vector2(point.X - c.X, point.Y - c.Y), new Vector2(c.Y - a.Y, a.X - c.X)) > 0;
